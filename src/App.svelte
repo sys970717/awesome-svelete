4125fabe68de4541006dbec223c32b85308bc1d2
Seoyeongsu <sys970717@naver.com>
2020-12-14 18:54:36 +0900

    todoapp 해보는 중.

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,27 @@
 <script>
-	export let name;
+	import Todo from './Todo.svelte';
+
+    let title = "";
+	let todos = [];
+	let id = 0;
+	
+	function createTodo() {
+		console.log(title);
+		todos.push({
+			id,
+			title,
+		});
+		todos = todos;
+		title = '';
+		id += 1;
+	};
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<input type="text" 
+	   bind:value={title}
+	   on:keydown={(e) => { e.key === 'Enter' && {createTodo()}} } />
+<button on:click={createTodo}> Cretae Todo </button>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+{#each todos as todo}
+	<Todo todo />
+{/each}
